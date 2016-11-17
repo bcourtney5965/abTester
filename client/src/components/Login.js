2e3b11823942ad7cb26f24e2a438cca5b44f5c42
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-11-16 17:59:26 -0800

    Broke central form out into its own component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
+import LoginForm from './loginForm';
 
 class Login extends Component {
-  constructor() {
-    super()
-    this.state = {
-      username: "",
-      password: ""
-    }
-  }
-  
-  handleChange(field, event){
-    const object = {};
-    object[field] = event.target.value;
-    this.setState(object);
-  }
-
   render() {
     return (
       <div>
         <button>facebookbutton</button>
-        <form>
-          <div>
-            <input id="username" type="text" placeholder="username" onChange={this.handleChange.bind(this, 'username')}></input>
-          </div>
-          <div>
-            <input id="password" type="text" placeholder="password" onChange={this.handleChange.bind(this, 'password')}></input>
-          </div>
-          <input type="submit" value="Login"></input>
-        </form>
+        <LoginForm />
         <a>Create an Account</a>
         <a>Forgot Password</a>
       </div>
