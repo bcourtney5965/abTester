85a286a6f1877eaf89e582eb348685504fb38eff
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-11-21 01:52:37 -0800

    Reworked data table, and broke code out into different components - not done

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,61 +1,73 @@
 import React, { Component } from 'react';
-import { DataTable } from 'react-data-components';
 
+class DashboardTableRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: "it works"
+    }
+  }
+  render() {
+    return (
+      <tr>
+        <td>{this.props.idea.pic}</td>
+        <td>{this.props.idea.name}</td>
+        <td>{this.props.idea.profit}</td>
+        <td>{this.props.idea.confidence}</td>
+        <td>{this.props.idea.lastUpdate}</td>
+      </tr>
+    );
+  }
+}
+
+class DashboardTable extends Component {
+  constructor() {
+    super();
+    this.state = {
+      test: "it works"
+    }
+  }
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>All</th>
+            <th>Project Name</th>
+            <th>Profit</th>
+            <th>Confidence Level</th>
+            <th>Last Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+          <DashboardTableRow idea={ideas} />
+        </tbody>
+      </table>
+    );
+  }
+}
+
+var ideas = [
+  {"pic": "pic", "name": "abTester", "profit": "$3,600", "confidence": "80%", "lastUpdate": "10/26/2016 10:30 PM"},
+  // {"pic": "pic", "name": "Funding Match-Maker", "profit": "$5,600", "confidence": "62%", "lastUpdate": "08/23/2016 08:12 AM"},
+  // {"pic": "pic", "name": "Automated Real Estate Finder", "profit": "$2,700", "confidence": "82%", "lastUpdate": "03/17/2016 03:39 PM"}
+]
 
 class Dashboard extends Component {
-
-
-
+  constructor() {
+    super();
+    this.state = {
+      test: "this works"
+    }
+  }
   render() {
-    var tableColumns = [
-        { title: 'Name', prop: 'name' },
-        { title: 'City', prop: 'city' },
-        { title: 'Street address', prop: 'street' },
-        { title: 'Phone', prop: 'phone', defaultContent: '<no phone>' }
-      ];
-
-    var data = [
-      {id: 1, name: 'Bob', city: 'San Francisco', street: '333 West Wacker Drive', phone: '321-7654' },
-      {id: 2, name: 'John', city: 'LA', street: '3 East 2nd Drive', phone: '321-7654' },
-      {id: 3, name: 'Steve', city: 'Austin', street: '53 1st Street', phone: '321-7654' }
-    ];
     return (
       <div id="dashboard">
-        <h1>Dashboard</h1>
-        <div id="dasboard-container">
-          <div id="controls" className="dasboard-container-children">
-            <fieldset>
-                <label className="selected">
-                  Working
-                  <input name="state" type="radio" value="working" checked />
-                </label>
-
-                <label>
-                  Standby
-                  <input name="state" type="radio" value="standby" />
-                </label>
-
-                <label className="warning">
-                  Off
-                  <input name="state" type="radio" value="off" />
-                </label>
-
-            </fieldset>
-            <div>
-            <button type="button" className="btn btn-success">+</button>
-            </div>
-          </div>
-          <DataTable
-            className="container"
-            keys="id"
-            columns={tableColumns}
-            initialData={data}
-            initialPageLength={5}
-            initialSortBy={{ prop: 'city', order: 'descending' }}
-            pageLengthOptions={[ 5, 20, 50 ]}
-            key={data.id}
-          />
-        </div>
+        <div id="selectionMenu">selectionMenu</div>
+        <input type="text"></input>
+        <button>Create New Project</button>
+        <DashboardTable ideas={ideas}/>
+        <div>data = {this.state.test}</div>
       </div>
     );
   }
